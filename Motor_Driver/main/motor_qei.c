eb698edb56986b1f812ca6833c894888654731ba
Ken Caluwaerts <ken@caluwaerts.eu>
2013-09-06 01:53:16 -0700

    some more qei debugging

--- Motor_Driver/main/motor_qei.c
+++ Motor_Driver/main/motor_qei.c
@@ -1,6 +1,7 @@
 #include "p33Exxxx.h"
 #include "motor_qei.h"
 #include "motor_state.h"
+#include "motor_led.h"
 
 qei_data qei_state;
     return_value_t qei_init()
@@ -17,11 +18,14 @@ qei_data qei_state;
         QEI1IOCbits.FLTREN = 0; //filter disabled for now
         QEI1IOCbits.SWPAB = 0; //do we need to swap A & B???
 
+        QEI1IOCbits.QEAPOL = 0;
+        QEI1IOCbits.QEAPOL = 0;
+
         POS1HLD = 0x0;
         POS1CNTL = 0x0;
 
-        INDX1HLD = 0x0;
-        INDX1CNTL = 0x0;
+        INDX1HLD = 0x1FFF;
+        INDX1CNTL = 0xFFFF;
 
         QEI1CONbits.QEIEN = 1; //activate encoder
         return qei_state.init_return;
@@ -31,7 +35,13 @@ qei_data qei_state;
     {
         qei_state.rotor_position = POS1CNTL; //always read low word first
         qei_state.rotor_position |= ((uint32_t)POS1HLD)<<16;
-        
+        uint32_t old_index = qei_state.index;
         qei_state.index = INDX1CNTL; //always read low word first
         qei_state.index |= ((uint32_t)INDX1HLD)<<16;
+        if(qei_state.index>old_index){
+            led_intensity_set(255,0,0,0);
+
+        }else if (qei_state.index<old_index){
+            led_intensity_set(0,0,0,255);
+        }
     }
