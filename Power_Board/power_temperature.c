ddaf1c5c1ed1558f3f7d2b50d5312a29c5b0f747
Ken Caluwaerts <ken@caluwaerts.eu>
2015-02-25 19:13:42 -0800

    power board is now copying temperature to CANOpen

--- Power_Board/power_temperature.c
+++ Power_Board/power_temperature.c
@@ -1,17 +1,22 @@
 #include "power_temperature.h"
 #include "power_i2c.h"
 #include "power_state.h"
+#include "power_can.h"
 
 temperature_data temperature_state;
 
 return_value_t temperature_init() {
     //I2C SHOULD HAVE BEEN INITIALIZED WHEN TRYING TO USE THIS MODULE
     temperature_state.temperature = 0;
-    temperature_state.state = RET_OK;
+    temperature_state.state = RET_ERROR;
     return RET_OK;
 }
 
 return_value_t temperature_update() {
+    if(temperature_state.state==RET_OK){
+        //copy to CANopen
+        CO(adc_state_C_temp) = temperature_state.temperature;
+    }
     if(temperature_state.state==RET_OK || temperature_state.state==RET_ERROR){
         //start a new read operation
         if(i2c_1_read(0b1001101,0x00,1,&temperature_state.temperature,&temperature_state.state)==RET_OK){
